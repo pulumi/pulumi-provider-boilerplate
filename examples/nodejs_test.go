3810a2c74dece272d4c93f12c86a7bb1a8e67083
Bryce Lampe <bryce@pulumi.com>
2025-05-30 16:02:32 -0700

    fixup node

--- examples/nodejs_test.go
+++ examples/nodejs_test.go
@@ -3,8 +3,22 @@
 
 package examples
 
-import "testing"
+import (
+	"testing"
 
-func TestNodeJS(t *testing.T) {
-	// TODO
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+)
+
+func TestNodejsExampleLifecycle(t *testing.T) {
+	t.Skip("linking isn't working correctly")
+
+	pt := pulumitest.NewPulumiTest(t, "nodejs",
+		opttest.YarnLink("@mynamespace/provider-boilerplate"),
+		opttest.AttachProviderServer("provider-boilerplate", providerFactory),
+	)
+
+	pt.Preview(t)
+	pt.Up(t)
+	pt.Destroy(t)
 }
